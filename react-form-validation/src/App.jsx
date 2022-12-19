f625432b96152e40221c02a21323b38b4c02c76a
MCCetin <mcan.cetin@mf.karaelmas.edu.tr>
2022-12-19 17:28:47 +0300

    folder structure changed

--- react-form-validation/src/App.jsx
+++ react-form-validation/src/App.jsx
@@ -1,9 +1,9 @@
-import LetterForm from "./components/LetterForm";
+import Form from "./components/Form"
 
 function App() {
   return (
     <div className="App">
-      <LetterForm />
+      <Form />
     </div>
   );
 }
